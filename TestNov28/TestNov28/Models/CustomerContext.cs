1c64e8540a686aea4d5f28bc3881149fa697f185
“DanielPenny” <Daniel.Penny@smu.ca>
2023-12-02 20:55:31 -0400

    Updated Sample From Now28
    Updated Sample From Now28

---
+++ TestNov28/TestNov28/Models/CustomerContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using InClassDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace InClassDemo.Models
+{
+    public partial class CustomerContext : DbContext
+    {
+        public CustomerContext()
+        {
+        }
+
+        public CustomerContext(DbContextOptions<CustomerContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.Addrses1).HasMaxLength(50);
+
+                entity.Property(e => e.City).HasMaxLength(50);
+
+                entity.Property(e => e.Country).HasMaxLength(50);
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+
+                entity.Property(e => e.Phone).HasMaxLength(50);
+
+                entity.Property(e => e.Postal).HasMaxLength(50);
+
+                entity.Property(e => e.Province).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
